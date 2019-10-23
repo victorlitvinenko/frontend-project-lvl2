42b80f2faf6b6e217a15eac637d8dca1281478bf
Victor Litvinenko <vitosik@gmail.com>
2019-10-23 12:07:07 +0300

    chore (gendiff.js) step #2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,5 +3,11 @@ const program = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((first, second) => {
+    const firstConfig = first;
+    const secondConfig = second;
+  })
   .parse(process.argv);
