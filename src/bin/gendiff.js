4a47cf47a600077f482a71b97c4d5edfd7f3726c
Victor Litvinenko <vitosik@gmail.com>
2019-10-23 11:28:30 +0300

    fix (gendiff.js) change commander.js command

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,4 +3,5 @@ const program = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .parse(process.argv);
