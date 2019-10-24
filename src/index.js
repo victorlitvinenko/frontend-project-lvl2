c5f0a6ea238790eb72b3f6378828f4c023ac9f5b
Victor Litvinenko <victor@inetlog.ru>
2019-10-24 11:44:44 +0300

    feature (index.js) step #3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-export default (pathToFile1, pathToFile2) => {
+const fs = require('fs');
+const _ = require('lodash');
 
+export default (pathToFile1, pathToFile2) => {
+  if (fs.existsSync(pathToFile1) && fs.existsSync(pathToFile2)) {
+    const object1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf8'));
+    const object2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf8'));
+    let result;
+    result = Object.keys(object1).reduce((acc, element) => {
+      if (_.has(object2, element)) {
+        if (object1[element] === object2[element]) {
+          return `${acc}    ${element}: ${object1[element]}\n`;
+        }
+        return `${acc}  + ${element}: ${object2[element]}\n  - ${element}: ${object1[element]}\n`;
+      }
+      return `${acc}  - ${element}: ${object1[element]}\n`;
+    }, '');
+    result = Object.keys(object2).reduce((acc, element) => {
+      if (!_.has(object1, element)) {
+        return `${acc}  + ${element}: ${object2[element]}\n`;
+      }
+      return acc;
+    }, result);
+    return `{\n${result}}`;
+  }
+  return false;
 };
