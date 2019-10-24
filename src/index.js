db4e1490b71dba0e7d7c766b285fc82e512deca0
Victor Litvinenko <victor@inetlog.ru>
2019-10-24 12:11:07 +0300

    refactor (index.js) replace let to const

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ export default (pathToFile1, pathToFile2) => {
   if (fs.existsSync(pathToFile1) && fs.existsSync(pathToFile2)) {
     const object1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf8'));
     const object2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf8'));
-    let result;
-    result = Object.keys(object1).reduce((acc, element) => {
+    const result = Object.keys(object2).reduce((acc, element) => {
+      if (!_.has(object1, element)) {
+        return `${acc}  + ${element}: ${object2[element]}\n`;
+      }
+      return acc;
+    }, Object.keys(object1).reduce((acc, element) => {
       if (_.has(object2, element)) {
         if (object1[element] === object2[element]) {
           return `${acc}    ${element}: ${object1[element]}\n`;
@@ -14,13 +18,7 @@ export default (pathToFile1, pathToFile2) => {
         return `${acc}  + ${element}: ${object2[element]}\n  - ${element}: ${object1[element]}\n`;
       }
       return `${acc}  - ${element}: ${object1[element]}\n`;
-    }, '');
-    result = Object.keys(object2).reduce((acc, element) => {
-      if (!_.has(object1, element)) {
-        return `${acc}  + ${element}: ${object2[element]}\n`;
-      }
-      return acc;
-    }, result);
+    }, ''));
     return `{\n${result}}`;
   }
   return false;
