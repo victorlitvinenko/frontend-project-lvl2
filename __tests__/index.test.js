dae6ba4548efd9f4039b27fd676ab04c4ad3dc23
Victor Litvinenko <victor@inetlog.ru>
2019-10-25 12:32:52 +0300

    refactor (index.test.js) change test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,5 +6,5 @@ test('genDiff', () => {
   const result = genDiff(`${__dirname}/__fixtures__/before.json`, `${__dirname}/__fixtures__/after.json`);
   const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf8');
   expect(result).toMatch(expected);
-  expect(genDiff()).toBe(false);
+  expect(genDiff()).toBeFalsy();
 });
