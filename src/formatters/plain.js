20e154eba0b7bb18632e959abdc6e0b221f76117
Victor Litvinenko <victor@inetlog.ru>
2019-11-05 12:40:18 +0300

    refactor (plain.js) rename const

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,14 +4,14 @@ const stringify = (value) => (_.isObject(value) ? '[complex value]' : `'${value}
 
 const render = (elements, parents = []) => {
   const result = elements.reduce((acc, elem) => {
-    const newName = [...parents, elem.name].join('.');
+    const fullName = [...parents, elem.name].join('.');
     switch (elem.type) {
       case 'deleted':
-        return `${acc}Property '${newName}' was removed\n`;
+        return `${acc}Property '${fullName}' was removed\n`;
       case 'added':
-        return `${acc}Property '${newName}' was added with value: ${stringify(elem.value)}\n`;
+        return `${acc}Property '${fullName}' was added with value: ${stringify(elem.value)}\n`;
       case 'changed':
-        return `${acc}Property '${newName}' was updated. From ${stringify(elem.value.old)} to ${stringify(elem.value.new)}\n`;
+        return `${acc}Property '${fullName}' was updated. From ${stringify(elem.value.old)} to ${stringify(elem.value.new)}\n`;
       case 'nested':
         return `${acc}${render(elem.value, [...parents, elem.name])}`;
       default:
