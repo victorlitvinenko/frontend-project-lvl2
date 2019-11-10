946c84b754839e441b89c35391a4999b21252a12
Victor Litvinenko <victor@inetlog.ru>
2019-11-10 17:25:32 +0300

    chore (gendiff.js) change version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import genDiff from '..';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('0.0.6')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((first, second) => {
