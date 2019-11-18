5ecf7660b277815bce3357ed438b357b2f443390
Victor Litvinenko <victor@inetlog.ru>
2019-11-18 12:14:12 +0300

    refactor (index.js)

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import render from './formatters';
 export default (pathToFile1, pathToFile2, format = 'nested') => {
   if (fs.existsSync(pathToFile1) && fs.existsSync(pathToFile2)) {
     const fileContent1 = fs.readFileSync(pathToFile1, 'utf8');
-    const fileContent2 = fs.readFileSync(pathToFile2, 'utf8');
     const fileType1 = getFileType(pathToFile1);
-    const fileType2 = getFileType(pathToFile2);
     const config1 = parse(fileContent1, fileType1);
+    const fileContent2 = fs.readFileSync(pathToFile2, 'utf8');
+    const fileType2 = getFileType(pathToFile2);
     const config2 = parse(fileContent2, fileType2);
     const ast = getAst(config1, config2);
     return render(ast, format);
